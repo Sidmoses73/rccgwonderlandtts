a1d57a9234fd8c7102af9c8edb94e048234e09e9
Ahonmisi Moses <79580586+Sidmoses73@users.noreply.github.com>
2026-02-08 21:31:51 +0100

    First commit
    Uploaded all files

---
+++ bible.js
@@ -0,0 +1,14 @@
+const BIBLE = {
+  "Genesis": [
+    "In the beginning God created the heaven and the earth...",
+    "Thus the heavens and the earth were finished..."
+  ],
+  "Psalms": [
+    "The Lord is my shepherd; I shall not want...",
+    "Why do the heathen rage, and the people imagine a vain thing?"
+  ],
+  "John": [
+    "In the beginning was the Word, and the Word was with God...",
+    "For God so loved the world, that he gave his only begotten Son..."
+  ]
+};
